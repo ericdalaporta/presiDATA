ea561e3c3a5211a45b875f01b141b988e5c72206
Eric Dala Porta <ericdasilvadalaporta@gmail.com>
2026-02-23 00:06:43 -0300

    remove: C:/Users/Rafaella Gostosa/Downloads/presiDATA-main/presiDATA-main/check_bolsonaro.js

--- C:/Users/Rafaella Gostosa/Downloads/presiDATA-main/presiDATA-main/check_bolsonaro.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const db = JSON.parse(fs.readFileSync('data/presidentes-db.json', 'utf8'));
-const p = db.presidentes.find(x => x.id === 'jair_bolsonaro');
-fs.writeFileSync('bolsonaro_check.txt', 
-    `SUCESSOS (${p.sucessos.length}):\n` + p.sucessos.map((s,i) => `${i+1}. ${s}`).join('\n') +
-    `\n\nPOLEMICAS (${p.polemicas.length}):\n` + p.polemicas.map((s,i) => `${i+1}. ${s}`).join('\n')
-);
-console.log('Done');
